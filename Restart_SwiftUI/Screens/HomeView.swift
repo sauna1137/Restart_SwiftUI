b7f652666cb50c8db281d151b41666d16820bec4
sauna1137 <subscription.search@gmail.com>
2024-07-03 07:13:07 +0900

    add some base views

---
+++ Restart_SwiftUI/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart_SwiftUI
+//
+//  Created by KS on 2024/07/02.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart_SwiftUI/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Restart_SwiftUI
+//
+//  Created by KS on 2024/07/02.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
